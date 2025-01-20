9379a8f1b6bdebf21d675b3e4e1cc1a31c12eb0c
shravani delta student <shravaniavula03@gmail.com>
2025-01-20 19:32:43 +0530

    home route

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const { faker } = require('@faker-js/faker');
 const mysql=require('mysql2');
+const express = require("express");
+const app =express();
+const path=require("path");
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
 
 const connection =  mysql.createConnection({
     host: 'localhost',
@@ -19,23 +25,31 @@ const connection =  mysql.createConnection({
     ];
   }
 
-let q="INSERT INTO user(id,username,email,password) VALUES ?";
-let data =[];
-for(let i =1 ;i<=100; i++){
-  data.push(getRandomUser());
-}
+// let q="INSERT INTO user(id,username,email,password) VALUES ?";
+// let data =[];
+// for(let i =1 ;i<=100; i++){
+//   data.push(getRandomUser());
+// }
 
-  try{
-    connection.query(q,[data],(err,result)=>{
+ 
+
+  // connection.end();
+app.get("/",(req,res)=>{
+  let q =`SELECT count(*)FROM user;`
+   try{
+    connection.query(q,(err,result)=>{
         if(err)throw err;
-        console.log(result);
+        let count=(result[0]["count(*)"]);
+        res.render("home.ejs",{count});
     });
 
   }catch(err){
     console.log(err);
+    res.send("success")
   }
+ 
+})
 
-  connection.end();
-
-
-//   console.log(createRandomUser());
+app.listen(8080,()=>{
+  console.log("server is listening to the port")
+})
